bb527b1d0cc4e3363ff69ccaeb662ea78a45e48b
SaurabheeShahi <saurabheeshahi23@gmail.com>
2026-07-16 10:33:54 +0545

    My to-do list app made with react

---
+++ src/MyComponents/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+export const About =() => {
+    return(
+        <div>
+            <h1>About my Todo List</h1>
+            <p> Artificial intelligence is becoming a useful part of everyday life, helping people complete tasks more efficiently and make informed decisions. Students use AI to understand difficult concepts, professionals rely on it to organize their work, and businesses use it to improve customer service. Despite its many advantages, AI works best when combined with human judgment, creativity, and ethical responsibility. As technology continues to evolve, learning how to use AI effectively will become an increasingly valuable skill in education, the workplace, and daily life.
+</p>
+        </div>
+    )
+}
+export default About;
